d38b7c6399547d56ac019f6aa072a96f0629cffb
MEI <1596944197@qq.com>
2025-04-10 18:26:03 +0800

    feat: 增加pwa配置

--- next.config.ts
+++ next.config.ts
@@ -1,7 +1,5 @@
-import type { NextConfig } from "next";
+import withPWA from "next-pwa";
 
-const nextConfig: NextConfig = {
-  /* config options here */
-};
-
-export default nextConfig;
+export default withPWA({
+  dest: "public",
+});
