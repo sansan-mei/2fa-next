717b5584de57dabb4216191dbe45364112ff3083
梅花三十三 <1596944197@qq.com>
2025-04-20 21:48:26 +0800

    refactor: lazy高阶组件接收fallback函数

--- src/components/_lazy.tsx
+++ src/components/_lazy.tsx
@@ -2,11 +2,12 @@ import { ComponentType, lazy, LazyExoticComponent, Suspense } from "react";
 
 // eslint-disable-next-line @typescript-eslint/no-explicit-any
 export default function _Lazy<T extends ComponentType<any>>(
-  load: () => Promise<{ default: T }>
+  load: () => Promise<{ default: T }>,
+  fallback?: React.ReactNode
 ): LazyExoticComponent<T> {
   const LazyComponent = lazy(load);
   const LazyComponentWrapper = (props: React.ComponentProps<T>) => (
-    <Suspense fallback={null}>
+    <Suspense fallback={fallback}>
       <LazyComponent {...props} />
     </Suspense>
   );
